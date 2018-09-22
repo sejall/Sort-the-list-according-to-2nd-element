d838f098990f71d0113072b40c493185079a05d4
sejall <33982474+sejall@users.noreply.github.com>
2018-09-22 06:29:41 +0530

    Add files via upload

---
+++ Sort the List According to the Second Element in Sublist.py
@@ -0,0 +1,11 @@
+# Sort the List According to the Second Element in Sublist 
+
+a=[['A',34],['B',21],['C',26]]
+for i in range(0,len(a)):
+    for j in range(0,len(a)-i-1):
+        if(a[j][1]>a[j+1][1]):
+            temp=a[j]
+            a[j]=a[j+1]
+            a[j+1]=temp
+ 
+print(a)
